e80a7d9abca1d91c2fdc9db48ee1ca97c1b4d746
RI381541 <rithesh.v82@wipro.com>
2018-12-18 20:25:59 +0530

    added stripe dependencies

--- routes/paymentService.js
+++ routes/paymentService.js
@@ -1,11 +1,26 @@
-var stripe = require('stripe')()
+var stripe = require('stripe')('sk_test_cOwzte92kN0aGAPN8t0bpxYt');
 
 
-stripe.transfers.create({
-    amount: 400,
-    currency: "usd",
-    destination: "acct_1032D82eZvKYlo2C",
-    transfer_group: "ORDER_95"
-  }, function(err, transfer) {
+// stripe.transfers.create({
+//     amount: 400,
+//     currency: "usd",
+//     destination: "acct_1032D82eZvKYlo2C",
+//     transfer_group: "ORDER_95"
+//   }, function(err, transfer) {
+//     // asynchronously called
+//   });
+
+
+  stripe.accounts.create({
+    type: 'custom',
+    country: 'US',
+    email: 'bob@example.com'
+  }, function(err, account) {
     // asynchronously called
+    if(!err){
+        console.log(account);
+    }
+    else {
+        console.log(err);
+    }
   });
